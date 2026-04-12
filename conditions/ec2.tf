7365af202a773b696a1d96623ce55d0f519dab3b
vishwanath44 <chinthavishwanath9@gmail.com>
2026-04-12 08:50:45 +0530

    added new files

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "example" {
   ami           = var.ami_id
   # if dev t3.micro, otherwise t3.small
-  instance_type = variable.environmet == "dev" ? "t3.micro" : "t3.small"
+  instance_type = var.envirnment == "dev" ? "t3.micro" : "t3.small"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
   tags = var.ec2_tags
